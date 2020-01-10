4f09a4231a08d90fbf017928dd6f2436d46c2116
danpatrickoneill <dan47573@gmail.com>
2020-01-09 21:10:51 -0500

    Initial setup for play style effects

--- classes/Match.py
+++ classes/Match.py
@@ -1,3 +1,9 @@
+STRATEGIES = {
+    'marksman': .8,
+    'bruiser': .2,
+    'balanced': .5
+}
+
 class Match:
 """
 Two players enter; both leave, but one leaves as the winner
@@ -5,9 +11,16 @@ Two players enter; both leave, but one leaves as the winner
     def __init__(self, user_player, opposing_player, score=11):
         self.user_player = user_player
         self.opposing_player = opposing_player
+        self.st
 
     def get_players(self):
         """
         Returns players involved in match instance
         """
         return [self.user_player, self.opposing_player]
+
+    def play_point(self):
+        user_strategy = STRATEGIES[self.user_player.style]
+        opp_strategy = STRATEGIES[self.opposing_player.style]
+
+        
