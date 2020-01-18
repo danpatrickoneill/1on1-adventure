34650efd86efcead18036c34f1b9ca814e25e345
danpatrickoneill <dan47573@gmail.com>
2020-01-17 21:14:09 -0500

    Set up redimentary one-shot game loop

--- game.py
+++ game.py
@@ -1,6 +1,22 @@
+import random
 from classes.player import Player
 from classes.match import Match
 
-player_one = Player("Geoff")
 
 print(player_one.skills)
+
+while True:
+    name = input("Please enter your name: ")
+    points = 0
+    player_one = Player(name)
+
+    factor = random.randrange(1,25)
+    if player_one.skills['three_point'] >= factor:
+        success_messages = ["It's good from downtown!", "Bang! Bang! Bang!", "Give them all three of those!"]
+        print(random.choice(success_messages))
+        points += 3
+    else:
+        failure_messages = ["Brick!", "That's way off from three!", "Off on the jumpshot"]
+        print(random.choice(failure_messages))
+    
+    print(f"{name} scored {points} points. Thanks for playing!")
