dc92d60460b7e00346b1aec5708087d98edd4019
danpatrickoneill <dan47573@gmail.com>
2020-01-18 16:11:28 -0500

    Commented out test loop

--- game.py
+++ game.py
@@ -2,22 +2,24 @@ import random
 from classes.player import Player
 from classes.match import Match
 
+potential opponents = []
 
-while True:
-    name = input("Please enter your name: ")
-    points = 0
-    player_one = Player(name)
-    if name in ["Steph", "Curry", "Klay", "Jesus", "Shuttlesworth"]:
-        player_one.skills['three_point'] = 20
+# Shootaround test app
+# while True:
+#     name = input("Please enter your name: ")
+#     points = 0
+#     player_one = Player(name)
+#     if name in ["Steph", "Curry", "Klay", "Jesus", "Shuttlesworth"]:
+#         player_one.skills['three_point'] = 20
 
-    factor = random.randrange(1,25)
-    if player_one.skills['three_point'] >= factor:
-        success_messages = ["It's good from downtown!", "Bang! Bang! Bang!", "Give them all three of those!"]
-        print(random.choice(success_messages))
-        points += 3
-    else:
-        failure_messages = ["Brick!", "That's way off from three!", "Off on the jumpshot"]
-        print(random.choice(failure_messages))
+#     factor = random.randrange(1,25)
+#     if player_one.skills['three_point'] >= factor:
+#         success_messages = ["It's good from downtown!", "Bang! Bang! Bang!", "Give them all three of those!"]
+#         print(random.choice(success_messages))
+#         points += 3
+#     else:
+#         failure_messages = ["Brick!", "That's way off from three!", "Off on the jumpshot"]
+#         print(random.choice(failure_messages))
     
-    print(f"{name} scored {points} points. Thanks for playing!")
-    break
+#     print(f"{name} scored {points} points. Thanks for playing!")
+#     break
